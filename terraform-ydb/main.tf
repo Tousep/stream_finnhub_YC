8f775941db23090fb320f22617e6c0ebcc727c99
Tousep <88712939+Tousep@users.noreply.github.com>
2024-01-18 20:19:25 +0300

    Update main.tf

--- terraform-ydb/main.tf
+++ terraform-ydb/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "${{ IAC_SACC_SECRET }}"
-  folder_id = "{{ env.iac_folder }}"
+  service_account_key_file = "${{ secret.IAC_SACC_SECRET }}"
+  folder_id = "${{ env.iac_folder }}"
   zone = "ru-central1-b"
 }
