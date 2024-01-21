3ce6c69de3f71e9bfb28eddc82ada3e8f8ed993e
Tousep <88712939+Tousep@users.noreply.github.com>
2024-01-21 13:32:01 +0300

    add backend

--- terraform-ydb/main.tf
+++ terraform-ydb/main.tf
@@ -5,6 +5,21 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "naa-s3-for-terraform-state"
+    region = "ru-central1"
+    key    = "resources/uat/main.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
 }
 
 provider "yandex" {
