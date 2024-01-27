e27b2df62d80f91b7e4dee271253def2edccdc2d
Tousep <88712939+Tousep@users.noreply.github.com>
2024-01-27 20:36:25 +0300

    efr

--- terraform-ydb/test/kafka.py
+++ terraform-ydb/test/kafka.py
@@ -5,7 +5,8 @@ from confluent_kafka import Consumer, Producer
 def kafka_check_topic(event, context):
     params = {
         'bootstrap.servers': 'rc1b-namn6f7m2172e4n9.mdb.yandexcloud.net:9091',
-        'security.protocol': 'SASL_PLAINTEXT',
+        'security.protocol': 'SASL_SSL',
+        'ssl.ca.location': './ca-certificates/Yandex/YandexInternalRootCA.crt',
         'sasl.mechanism': 'SCRAM-SHA-512',
         'sasl.username': 'finnhub',
         'sasl.password': 'finnhub',
