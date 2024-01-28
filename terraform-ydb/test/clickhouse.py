e95b5d099df2ccf4a10f422f53c72ad08b742d06
Tousep <88712939+Tousep@users.noreply.github.com>
2024-01-28 15:58:18 +0300

    add clichouse check

---
+++ terraform-ydb/test/clickhouse.py
@@ -0,0 +1,25 @@
+from confluent_kafka import Consumer, Producer
+import time
+import os
+
+def clickhouse_connection_check(event, context):
+    import requests
+
+    response = requests.get(
+        'https://{0}.mdb.yandexcloud.net:8443'.format('rc1b-063qep40gn13ubgg'),
+        params={
+            'query': 'SELECT version()',
+        },
+        headers={
+            'X-ClickHouse-User': 'finnhub_cl',
+            'X-ClickHouse-Key': 'finnhub_cl',
+        },
+        verify='./ca-certificates/Yandex/YandexInternalRootCA.crt'
+    )
+
+    response.raise_for_status()
+    print(response.text)
+
+
+if __name__ == '__main__':
+    clickhouse_connection_check(1,2)
