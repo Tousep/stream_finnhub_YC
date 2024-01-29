6724d5044cefef3a8a68cb1e308006f8424fe636
Tousep <88712939+Tousep@users.noreply.github.com>
2024-01-29 21:48:29 +0300

    s

--- terraform-ydb/test/clickhouse.py
+++ terraform-ydb/test/clickhouse.py
@@ -6,29 +6,29 @@ from clickhouse_driver import Client
 
 def clickhouse_connection_check(event, context):
 
-    client = Client(host='rc1b-cskcq8u3ogqc385s.mdb.yandexcloud.net',
-                    user='finnhub_cl',
-                    password='finnhub_cl',
-                    port=9000,
-                    secure=True,
-                    verify=True,
-                    ca_certs='./ca-certificates/Yandex/RootCA.crt')
+    # client = Client(host='rc1b-cskcq8u3ogqc385s.mdb.yandexcloud.net',
+    #                 user='finnhub_cl',
+    #                 password='finnhub_cl',
+    #                 port=9000,
+    #                 secure=True,
+    #                 verify=True,
+    #                 ca_certs='./ca-certificates/Yandex/RootCA.crt')
 
-    print(client.execute('SELECT version()'))
-    # response = requests.get(
-    #     'https://{0}:8443'.format('rc1b-063qep40gn13ubgg.mdb.yandexcloud.net'),
-    #     params={
-    #         'query': 'SELECT version()',
-    #     },
-    #     headers={
-    #         'X-ClickHouse-User': 'finnhub_cl',
-    #         'X-ClickHouse-Key': 'finnhub_cl',
-    #     },
-    #     verify='./ca-certificates/Yandex/YandexInternalRootCA.crt'
-    # )
+    # print(client.execute('SELECT version()'))
+    response = requests.get(
+        'https://{0}:8443'.format('rc1b-cskcq8u3ogqc385s.mdb.yandexcloud.net'),
+        params={
+            'query': 'SELECT version()',
+        },
+        headers={
+            'X-ClickHouse-User': 'finnhub_cl',
+            'X-ClickHouse-Key': 'finnhub_cl',
+        },
+        verify='./ca-certificates/Yandex/YandexInternalRootCA.crt'
+    )
 
-    # response.raise_for_status()
-    # print(response.text)
+    response.raise_for_status()
+    print(response.text)
 
 
 if __name__ == '__main__':
